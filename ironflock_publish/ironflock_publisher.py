d7d5c47a75ddd8318ca29f2b5989f64ae8a14e83
marko <marko.petzold@record-evolution.de>
2025-05-09 14:41:20 +0200

    iroinflock bridge

--- ironflock_publish/ironflock_publisher.py
+++ ironflock_publish/ironflock_publisher.py
@@ -3,9 +3,16 @@ import json
 import base64
 import struct
 import os
+import sys
 from ironflock import IronFlock
 import paho.mqtt.client as mqtt
-from lora_device_simulator import simulate_device
+
+# from lora_device_simulator import simulate_device
+
+import logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 MQTT_BROKER = "mosquitto"
 MQTT_PORT = 1883
@@ -15,25 +22,34 @@ ENABLE_DEMO_DATA = (os.environ.get('ENABLE_DEMO_DATA', 'false') == 'true')
 def on_connect(client, userdata, flags, rc, properties):
     print(f"Connected with result code {rc}")
     # Subscribe to the uplink topic
-    client.subscribe(f"application/{APPLICATION_ID}/device/+/event/up")
+    client.subscribe(f"application/+/device/+/event/up")
 
 async def on_message(client, userdata, msg):
     try:
         data = json.loads(msg.payload.decode("utf-8"))
-        payload = transform_payload(data) #your transform payload function.
+        topic_parts = msg.topic.split('/')
+        application_id = None
+        if len(topic_parts) > 1 and topic_parts[0] == "application":
+            application_id = topic_parts[1]
+            # logger.debug(f"Extracted Application ID: {application_id}") # Use debug for less verbose logging
+        else:
+            logger.warning(f"Could not extract Application ID from topic: {msg.topic}")
+
+        payload = transform_payload(data, application_id) #your transform payload function.
         await ironflock.publish_to_table('sensordata', [payload])
 
     except Exception as e:
         print(f"Error processing message: {e}")
 
-async def transform_payload(data):
+async def transform_payload(data, application_id):
     """Processes uplink data from ChirpStack and transforms the payload."""
-    print("received raw data", data)
+    logger.info("received raw data", data)
     try:
         
         transformed_data = {
             "tsp": data.get("publishedAt"),
-            "devEUI": data.get("devEUI"),
+            "application_id": application_id,
+            "dev_eui": data.get("devEUI"),
             "fPort": data.get("fPort"),
             "data": base64.b64decode(data.get("data", "")).decode("utf-8") if data.get("data") else None,  # Decode Base64
             "dr": data.get("txInfo", {}).get("dr"),
@@ -58,8 +74,8 @@ async def main():
     client.on_message = on_message
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-    if ENABLE_DEMO_DATA:
-        task2 = asyncio.create_task(simulate_device())
+    # if ENABLE_DEMO_DATA:
+    #     task2 = asyncio.create_task(simulate_device())
     client.loop_start()
 
 if __name__ == "__main__":
