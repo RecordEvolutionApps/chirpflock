7496176761eafd2e089877fd1d3bb7b9bc52938b
marko <marko.petzold@record-evolution.de>
2025-05-30 18:01:45 +0200

    add certs

--- configuration/chirpstack/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/brocaar/lorawan-devices /tmp/lorawan-devices
-chirpstack -c /etc/chirpstack import-legacy-lorawan-devices-repository -d /tmp/lorawan-devices
-
-exec /usr/bin/chirpstack -c /etc/chirpstack
---
+++ configuration/chirpstack/generate_certs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CERT_DIR=/etc/chirpstack/certs
+CA_KEY=$CERT_DIR/ca.key
+CA_CERT=$CERT_DIR/ca.crt
+
+mkdir -p "$CERT_DIR"
+
+# Generate CA cert if not present
+if [ ! -f "$CA_KEY" ] || [ ! -f "$CA_CERT" ]; then
+  echo "[INFO] Generating CA certificate..."
+  openssl genrsa -out "$CA_KEY" 4096
+  openssl req -x509 -new -nodes -key "$CA_KEY" -sha256 -days 1825 -out "$CA_CERT" \
+    -subj "/C=DE/ST=Hessen/L=Frankfurt/O=IronFlock/CN=IronFlock CA"
+else
+  echo "[INFO] Using existing CA certificate."
+fi
